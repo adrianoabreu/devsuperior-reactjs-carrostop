f267b651c806d5faa43d774aecc71972e1436b9a
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-08 11:43:48 -0300

    ReactJS - Carros Top estruturando pagina de catalogo com componentes Header, Searchbar e responsividade

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './components/Header';
+import Catalog from './routes/Catalog';
 
 function App() {
 
   return (
-    <Header/>
+    <>
+      <Catalog/>
+    </>
   );
 }
 
