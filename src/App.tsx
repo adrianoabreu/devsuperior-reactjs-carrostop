b2d0943f75ccea24fe8f741b7f6142730c903407
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-06-08 12:57:57 -0300

    ReactJS - Estilizando botao comprar na tela de catalogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Catalog from './routes/Catalog';
 
 function App() {
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,7 +2,7 @@ import './styles.css';
 
 export default function Button() {
     return (
-        <div>
+        <div className="ct-button">
             Comprar
         </div>
     );
